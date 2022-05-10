b2c0120d4998a05c4679fbd63d1240ed2709fdc6
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-05-10 14:20:01 +0300

    gamybos

--- 004/app.js
+++ 004/app.js
@@ -1,16 +1,12 @@
-'use strict';
+"use strict";
 
-
-const n = ['Petras', 'Asilas', 'Barsukas', 'Kate'];
-
+const n = ["Petras", "Asilas", "Barsukas", "Kate"];
 
 console.table(n);
 
-
 console.log(n[2]);
 
-
-n[1] = 'Kazys';
+n[1] = "Kazys";
 
 // n[8] = 'Suo Sarikas';
 
@@ -18,42 +14,52 @@ n[1] = 'Kazys';
 
 // n.shift();
 
+n.push("Suo Sarikas");
 
-n.push('Suo Sarikas');
-
-n.unshift('Pele');
-
+n.unshift("Pele");
 
 // n.splice(2, 1);
-
 
 console.table(n);
 
 console.log(n.length);
 
-
 for (let i = 0; i < n.length; i++) {
-    console.log(n[i]); // i= 0, 1, 2, ....
+  console.log(n[i]); // i= 0, 1, 2, ....
 }
 
 const nPlus = [
-    [4, 7, 12],
-    [85, 17, 12],
-    [10, 33, 74],
-    [26, 41, 99],
-]
+  [4, 7, 12],
+  [85, 17, 12],
+  [10, 33, 74, 14],
+  [26, 41, 99, 77, 99],
+];
 
 console.table(nPlus);
 
-console.log(nPlus[3][2]);
-
+// console.log(nPlus[3][2]);
 
 for (let i = 0; i < nPlus.length; i++) {
-
-    for (let k = 0; k < nPlus[i].length; k++) {
-
-        console.log(nPlus[i][k]); 
-
-    }
-
+  for (let k = 0; k < nPlus[i].length; k++) {
+    console.log(nPlus[i][k]);
+  }
 }
+
+function rand(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+// nPlus.forEach(m => m.forEach(k => console.log(k)));
+
+const naujasNamas = [];
+
+for (let k = 0; k < 10; k++) {
+  const aukstas = [];
+  for (let i = 0; i < 10; i++) {
+    aukstas.push(rand(10, 99));
+  }
+  naujasNamas.push(aukstas);
+}
+
+console.table(naujasNamas);
