939926dc81fdedff8daf852f91e40d7b14f41cc7
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-05-10 13:58:31 +0300

    masyvas masyve

--- 004/app.js
+++ 004/app.js
@@ -1,3 +1,6 @@
+'use strict';
+
+
 const n = ['Petras', 'Asilas', 'Barsukas', 'Kate'];
 
 
@@ -30,6 +33,27 @@ console.log(n.length);
 
 
 for (let i = 0; i < n.length; i++) {
-
     console.log(n[i]); // i= 0, 1, 2, ....
+}
+
+const nPlus = [
+    [4, 7, 12],
+    [85, 17, 12],
+    [10, 33, 74],
+    [26, 41, 99],
+]
+
+console.table(nPlus);
+
+console.log(nPlus[3][2]);
+
+
+for (let i = 0; i < nPlus.length; i++) {
+
+    for (let k = 0; k < nPlus[i].length; k++) {
+
+        console.log(nPlus[i][k]); 
+
+    }
+
 }
