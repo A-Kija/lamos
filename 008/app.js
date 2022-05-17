0882c91e40c12d58657551cdb33ef1247239664b
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-05-17 13:21:16 +0300

    kodas

--- 008/app.js
+++ 008/app.js
@@ -8,7 +8,7 @@ const monsterSize = rand(10, 30);
 
 const makeNiceArray = () => {
   const array = [];
-  const size = rand(10, 20);
+  const size = rand(1, 2);
   for (let i = 0; i < size; i++) {
     array.push(rand(0, 10));
   }
@@ -26,3 +26,65 @@ for (let sizeNumber = 0; sizeNumber < monsterSize; sizeNumber++) {
     monster = monsterPart;
 }
 console.log(monster);
+
+const prime = (a) => {
+    let count = 0;
+    for (let i = 2; i < a; i++) {
+        if (a % i === 0) {
+            count++;
+        }
+    }
+    return count;
+};
+
+const sqArray = [];
+
+for (let i = 0; i < 10; i++) {
+    const row = [];
+    for (let y = 0; y < 10; y++) {
+        row.push(rand(1, 100));
+    }
+    sqArray.push(row);
+}
+
+const primeSum = a => {
+    let sum = 0;
+    let count = 0;
+    a.forEach(e => e.forEach(v => {
+        if (!prime(v)) {
+            count++;
+            sum += v;
+        }
+    }));
+    return count ? sum / count : 0;
+}
+
+const findMin = a => {
+    let min = a[0][0];
+    let coord = [0, 0];
+    a.forEach((r, ri) => r.forEach((v, ci) => {
+        if (v < min) {
+            min = v;
+            coord = [ri, ci];
+        }
+    }));
+    return coord;
+}
+
+console.table(sqArray);
+
+console.log(findMin(sqArray));
+
+let s = 1000;
+
+while(primeSum(sqArray) < 70){
+    if(--s === 0) {
+        console.log('BR')
+        break;
+    }
+    const [x, y] = findMin(sqArray);
+    sqArray[x][y] += 3;
+
+}
+
+console.table(sqArray);
