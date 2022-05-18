6f584759143fee645498e20835e57ee3f49619a8
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-05-18 14:25:51 +0300

    skaicius

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -2,15 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Hello from './Components/009/Hello';
 import Kurmis from './Components/009/Kurmis';
-
+const k = 5;
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Hello/>
-        <Hello></Hello>
-        <Hello></Hello>
-        <Kurmis></Kurmis>
+        <Hello spalva="green" bg="yellow"/>
+        <Hello spalva="black" bg="white"></Hello>
+        <Hello spalva="gray" bg="pink"></Hello>
+        <Kurmis skaicius={6}></Kurmis>
       </header>
     </div>
   );
--- r1/src/Components/009/Hello.jsx
+++ r1/src/Components/009/Hello.jsx
@@ -1,10 +1,10 @@
-function Hello() {
+function Hello({spalva, bg}) {
 
     return (
         <h2 style={
             {
-                color: 'pink',
-                backgroundColor: 'skyblue',
+                color: spalva,
+                backgroundColor: bg,
                 padding: '7px'
             }
         }>Hello</h2>
