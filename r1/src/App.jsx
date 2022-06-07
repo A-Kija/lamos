53cf3b32af0769382a5cda4196828bad655de803
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-06-07 13:02:28 +0300

    payload

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,13 +1,15 @@
 import { useReducer } from 'react';
 import './App.scss';
 import colorReducer from './Reducers/colorReducer';
+import numberReducer from './Reducers/numberReducer';
+import rand from './Functions/rand';
  
 
 function App() {
 
     // const [color, setColor] = useState('yellow');
-    const [color, dispachColor] = useReducer(colorReducer, 'yellow')
-
+    const [color, dispachColor] = useReducer(colorReducer, 'yellow');
+    const [numb, dispachNumb] = useReducer(numberReducer, '0000');
     // const goPink = () => {
     //     setColor('pink');
     // }
@@ -33,15 +35,36 @@ function App() {
         }
         dispachColor(action);
     }
+
+    const number1 = () => {
+        const action = {
+            type: 'do1'
+        }
+        dispachNumb(action);
+    }
+
+    const number2 = () => {
+        const action = {
+            type: 'do2',
+            payload: rand(0, 100)
+        }
+        dispachNumb(action);
+    }
     
     return (
         <div className="App">
           <header className="App-header">
-           <h1 style={{backgroundColor: color}}>Welcome to Reducer</h1>
+           <h1 style={{backgroundColor: color}}>Welcome to Reducer
+           <span> {numb} </span>
+           </h1>
             <div className="kvc">
             <button onClick={goPink}>Go pink</button>
             <button onClick={goYellow}>Go yellow</button>
             <button onClick={goChange}>Go and Change</button>
+            </div>
+            <div className="kvc">
+            <button onClick={number1}>Go One</button>
+            <button onClick={number2}>Go Two</button>
             </div>
           </header>
         </div>
