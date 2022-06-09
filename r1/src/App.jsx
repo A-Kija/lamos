9a39ecfd98c13de0f437b081f7032bf94b591f2e
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-06-09 13:14:17 +0300

    reducer

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,22 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import './App.scss';
 import axios from 'axios';
+import booksReducer from './Reducers/booksReducer';
 
 function App() {
 
-    const [books, setBooks] = useState([]);
+    // const [books, setBooks] = useState([]);
+    const [books, dispachBooks] = useReducer(booksReducer, []);
 
     useEffect(() => {
         axios.get('http://in3.dev/knygos/')
         .then(res => {
-            setBooks(res.data)
+            dispachBooks({payload:res.data, type:'get_from_server'})
         })
     }, []);
 
     return (
         <div className="App">
             <header className="App-header">
-                <h1>REDUCER</h1>
+                <h1>BOOKS REDUCER</h1>
                 <div>
                     {
                        books.length ? books.map(b => <div key={b.id}>{b.title}</div>) : <h2>Loading...</h2>
