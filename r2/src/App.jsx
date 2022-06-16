8d81da56210318ae92595d8157e26640b7d66731
A-Kija <arvydas.kijakauskas@gmail.com>
2022-06-16 12:42:51 +0300

    update

--- r2/src/App.jsx
+++ r2/src/App.jsx
@@ -3,71 +3,68 @@ import './bootstrap.css';
 import './crud.scss';
 import Create from './Components/Create';
 import List from './Components/List';
-
 import Edit from './Components/Edit';
-// import './App.scss';
-
-
+import TreeContext from './Components/TreeContext';
 
 function App() {
 
-    const [lastUpdate, setLastUpdate] = useState(Date.now());
+  const [lastUpdate, setLastUpdate] = useState(Date.now());
 
-    const [exes, setExes] = useState(null);
-    const [modalData, setModalData] = useState(null);
+  const [trees, setTrees] = useState(null);
+  const [modalData, setModalData] = useState(null);
 
-    const [createData, setCreateData] = useState(null);
-    const [deleteData, setDeleteData] = useState(null);
-    const [editData, setEditData] = useState(null);
+  const [createData, setCreateData] = useState(null);
+  const [deleteData, setDeleteData] = useState(null);
+  const [editData, setEditData] = useState(null);
 
-    //Read
-    useEffect(() => {
-       
-    }, [lastUpdate]);
+  //Read
+  useEffect(() => {
 
-    // Create
-    useEffect(() => {
-        if (null === createData) {
-            return;
-        }
-        
-        setLastUpdate(Date.now());
-    }, [createData]);
+  }, [lastUpdate]);
 
-    // Delete
-    useEffect(() => {
-        if (null === deleteData) {
-            return;
-        }
-       
-        setLastUpdate(Date.now());
-    }, [deleteData]);
+  // Create
+  useEffect(() => {
+    if (null === createData) return;
 
-    // Edit
-    useEffect(() => {
-        if (null === editData) {
-            return;
-        }
 
-        setLastUpdate(Date.now());
-    }, [editData]);
 
-    
-    return (
-        <>
-            <div className="container">
-                <div className="row">
-                    <div className="col-4">
-                        <Create setCreateData={setCreateData}></Create>
-                    </div>
-                    <div className="col-8">
-                        <List exes={exes} setDeleteData={setDeleteData} setModalData={setModalData}></List>
-                    </div>
-                </div>
-            </div>
-            <Edit setEditData={setEditData} modalData={modalData} setModalData={setModalData}></Edit>
-        </>
-    );
+    setLastUpdate(Date.now());
+  }, [createData]);
+
+  // Delete
+  useEffect(() => {
+    if (null === createData) return;
+
+    setLastUpdate(Date.now());
+  }, [deleteData]);
+
+  // Edit
+  useEffect(() => {
+    if (null === createData) return;
+
+    setLastUpdate(Date.now());
+  }, [editData]);
+
+
+  return (
+    <TreeContext.Provider value={
+      {
+        trees
+      }
+    }>
+      <div className="container">
+        <div className="row">
+          <div className="col-4">
+            <Create setCreateData={setCreateData}></Create>
+          </div>
+          <div className="col-8">
+            <List trees={trees} setDeleteData={setDeleteData} setModalData={setModalData}></List>
+          </div>
+        </div>
+      </div>
+      <Edit setEditData={setEditData} modalData={modalData} setModalData={setModalData}></Edit>
+    </TreeContext.Provider>
+  );
 
 
 }
