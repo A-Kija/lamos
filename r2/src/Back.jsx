ad446a3ea573e0649317f1da6b76f9d233e8c393
A-Kija <arvydas.kijakauskas@gmail.com>
2022-06-28 13:14:29 +0300

    as

--- r2/src/Back.jsx
+++ r2/src/Back.jsx
@@ -33,6 +33,11 @@ function Back() {
   const [message, setMessage] = useState(null);
   const [disableCreate, setDisableCreate] = useState(false);
 
+
+  useEffect(() => {
+    // setInterval(() => setLastUpdate(Date.now()), 3000);
+  }, []);
+
 //////////////////TREES?/////////////////////////////
   //Read
   useEffect(() => {
