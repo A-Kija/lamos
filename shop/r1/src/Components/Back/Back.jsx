e358e6a1dea61a48e63bb46da4082207ba38aead
A-Kija <arvydas.kijakauskas@gmail.com>
2022-06-29 14:56:38 +0300

    vienas

--- shop/r1/src/Components/Back/Back.jsx
+++ shop/r1/src/Components/Back/Back.jsx
@@ -1,12 +1,36 @@
-import { useState } from 'react';
+import { useState,  useEffect} from 'react';
 import BackContext from './BackContext';
 import CatsCrud from './Cats/Crud';
 import Nav from './Nav';
 import ProductsCrud from './Products/Crud';
+import axios from 'axios';
 
 function Back({ show }) {
 
+    const [lastUpdate, setLastUpdate] = useState(Date.now());
+
+
     const [createCat, setCreateCat] = useState(null);
+
+
+
+
+    // Create
+    useEffect(() => {
+        if (null === createCat) return;
+        axios.post('http://localhost:3003/admin/cats', createCat)
+            .then(res => {
+                showMessage(res.data.msg);
+                setLastUpdate(Date.now());
+            })
+            .catch(error => {
+                showMessage({ text: error.message, type: 'danger' });
+            })
+    }, [createCat]);
+
+    const showMessage = () => {
+
+    }
 
 
     return (
--- shop/server/app.js
+++ shop/server/app.js
@@ -18,6 +18,20 @@ const con = mysql.createConnection({
     database: "la_ma_shop",
 });
 
+
+// CATS
+app.post("/admin/cats", (req, res) => {
+    const sql = `
+    INSERT INTO cats
+    (title)
+    VALUES (?)
+    `;
+    con.query(sql, [req.body.title], (err, result) => {
+        if (err) throw err;
+        res.send({ result, msg: { text: 'OK, new Cat was created', type: 'success' } });
+    });
+});
+
 app.listen(port, () => {
     console.log(`Bebras klauso porto Nr ${port}`);
 });
