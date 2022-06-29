369eef216f10d6abb32139a9472a810f12da2210
A-Kija <arvydas.kijakauskas@gmail.com>
2022-06-29 14:32:08 +0300

    create

--- shop/r1/src/Components/Back/Back.jsx
+++ shop/r1/src/Components/Back/Back.jsx
@@ -1,26 +1,28 @@
+import { useState } from 'react';
+import BackContext from './BackContext';
 import CatsCrud from './Cats/Crud';
 import Nav from './Nav';
 import ProductsCrud from './Products/Crud';
 
 function Back({ show }) {
 
-    if (show === 'admin') {
-        return (
-            <>
-            <Nav/>
-            <h1>BACK</h1>
-            </>
-        )
-    }
-    if (show === 'cats') {
-        return (
-            <CatsCrud/>
-        )
-    }
-    if (show === 'products') {
-        return (
-            <ProductsCrud/>
-        )
-    }
+    const [createCat, setCreateCat] = useState(null);
+
+
+    return (
+        <BackContext.Provider value={{
+            setCreateCat
+        }}>
+            {
+                show === 'admin' ?
+                    <>
+                        <Nav />
+                        <h1>BACK</h1>
+                    </>
+                    : show === 'cats' ? <CatsCrud /> :
+                        show === 'products' ? <ProductsCrud /> : null
+            }
+        </BackContext.Provider>
+    )
 }
 export default Back;
--- shop/r1/src/Components/Back/Nav.jsx
+++ shop/r1/src/Components/Back/Nav.jsx
@@ -1,28 +1,34 @@
-import {NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Nav() {
 
     return (
-        <nav className="nav">
-            <NavLink to="/admin/" className="nav-link" style={
-                ({ isActive }) =>
-                isActive ? {
-                    color: 'crimson'
-                } : null
-            }>Admin</NavLink>
-            <NavLink to="/admin/cats" className="nav-link"style={
-                ({ isActive }) =>
-                isActive ? {
-                    color: 'crimson'
-                } : null
-            }>Categories</NavLink>
-            <NavLink to="/admin/products" className="nav-link"style={
-                ({ isActive }) =>
-                isActive ? {
-                    color: 'crimson'
-                } : null
-            }>Products</NavLink>
-        </nav>
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <nav className="nav">
+                        <NavLink to="/admin/" className="nav-link" style={
+                            ({ isActive }) =>
+                                isActive ? {
+                                    color: 'crimson'
+                                } : null
+                        }>Admin</NavLink>
+                        <NavLink to="/admin/cats" className="nav-link" style={
+                            ({ isActive }) =>
+                                isActive ? {
+                                    color: 'crimson'
+                                } : null
+                        }>Categories</NavLink>
+                        <NavLink to="/admin/products" className="nav-link" style={
+                            ({ isActive }) =>
+                                isActive ? {
+                                    color: 'crimson'
+                                } : null
+                        }>Products</NavLink>
+                    </nav>
+                </div>
+            </div>
+        </div>
     )
 }
 
