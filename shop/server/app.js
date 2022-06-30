194112ba9587591342a47e23eadb3a47795efd39
A-Kija <arvydas.kijakauskas@gmail.com>
2022-06-30 14:06:22 +0300

    update

--- shop/server/app.js
+++ shop/server/app.js
@@ -55,6 +55,21 @@ app.delete("/admin/cats/:id", (req, res) => {
     });
 });
 
+app.put("/admin/cats/:id", (req, res) => {
+    const sql = `
+    UPDATE cats
+    SET title = ?
+    WHERE id = ?
+    `;
+    con.query(sql, [req.body.title, req.params.id], (err, result) => {
+        if (err) throw err;
+        res.send({ result, msg: { text: 'OK, Cat updated. Now it is as new', type: 'success' } });
+    });
+});
+
+
+
+
 
 
 
